f2438418f1d4918d23d6dda9fb715ba5e7e474f8
智兵 <369642339@qq.com>
2022-06-08 14:47:30 +0800

    subcode1

--- app/router.js
+++ app/router.js
@@ -4,8 +4,8 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
-  router.get('/', controller.home.index);
+	const { router, controller } = app;
+	router.get('/', controller.home.index);
 
-  router.post('/drawImage', controller.home.draw)
+	router.post('/drawImage', controller.home.draw);
 };
