c1f19ac622194db822d5dd8cea7fd5c66624ee06
稍等片刻码上就好 <464801095@qq.com>
2023-06-27 16:28:13 +0800

    fix: 修改印章样式

--- app/service/tradeCertificate.js
+++ app/service/tradeCertificate.js
@@ -1,6 +1,6 @@
 'use strict';
 const Service = require('egg').Service;
-
+const path = require('path');
 class TradeCertificateService extends Service {
 
 	// 绘制标题
@@ -41,7 +41,7 @@ class TradeCertificateService extends Service {
 
 		const structArr = [
 			[{ field: '合作主体', value: collaborate }, {}], 
-			[{ field: '付款日期', value: paymentDate }, { field: '付款金额', value: `${payFee}   ${payFeeChinese}` }],
+			[{ field: '付款日期', value: paymentDate }, { field: '付款金额', value: `${payFee || ''}   ${payFeeChinese || ''}` }],
 			[{ field: '车架号', value: vinCode }, { field: '车牌号', value: licenseCode }],
 			[{ field: '收款方', value: holder }, { field: '付款方', value: payCompany }],
 			[{ field: '收款方账号', value: holderAccountNumber }, { field: '付款方账号', value: payAccountNumber }],
@@ -63,61 +63,10 @@ class TradeCertificateService extends Service {
 	// 绘制印章
 	drawSeal(canvasCtx, config) {
 		const { cellWidth, cellHeight, } = config;
-    
-		// 绘制印章边框     
-		canvasCtx.lineWidth = 7;  
-		canvasCtx.strokeStyle = '#f54848';  
-		canvasCtx.beginPath();  
-		canvasCtx.arc(6 * cellWidth, 200 + 6 * cellHeight, 120, 0, Math.PI * 2);  
-		canvasCtx.stroke();  
-  
-		// 画五角星  
-		create5star(canvasCtx, 6 * cellWidth, 200 + 6 * cellHeight, 20, '#f00', 0);  
-
-		// 绘制印章单位    
-		const sealText = '上海常信拍卖有限公司'; 
-		canvasCtx.lineWidth = 2;
-		canvasCtx.translate(6 * cellWidth, 200 + 6 * cellHeight,);// 平移到此位置,  
-		canvasCtx.font = '25px';  
-		const count = sealText.length;// 字数     
-		const angle = 4 * Math.PI / (3 * (count - 1));// 字间角度     
-		const chars = sealText.split('');     
-		let c;  
-		for (let i = 0; i < count; i++) {    
-			c = chars[i];// 需要绘制的字符     
-			if (i === 0) canvasCtx.rotate(5 * Math.PI / 6);  
-			else { canvasCtx.rotate(angle); }//   
-			canvasCtx.save();   
-			canvasCtx.translate(90, 0);// 平移到此位置,此时字和x轴垂直     
-			canvasCtx.rotate(Math.PI / 2);// 旋转90度,让字平行于x轴     
-			canvasCtx.strokeText(c, 0, 0);// 此点为字的中心点     
-			canvasCtx.restore();               
-		}     
-  
-		// 绘制五角星    
-		/**  
-     * 创建一个五角星形状. 该五角星的中心坐标为(sx,sy),中心到顶点的距离为radius,rotate=0时一个顶点在对称轴上  
-     * rotate:绕对称轴旋转rotate弧度  
-     */    
-		function create5star(canvasCtx, sx, sy, radius, color, rotato) {    
-			canvasCtx.save();    
-			canvasCtx.fillStyle = color;    
-			canvasCtx.translate(sx, sy);// 移动坐标原点    
-			canvasCtx.rotate(Math.PI + rotato);// 旋转    
-			canvasCtx.beginPath();// 创建路径    
-			let x = Math.sin(0);    
-			let y = Math.cos(0);    
-			const dig = Math.PI / 5 * 4;    
-			for (let i = 0; i < 5; i++) { // 画五角星的五条边    
-				 x = Math.sin(i * dig);    
-				 y = Math.cos(i * dig);    
-				canvasCtx.lineTo(x * radius, y * radius);    
-			}     
-			canvasCtx.closePath();    
-			canvasCtx.stroke();    
-			canvasCtx.fill();    
-			canvasCtx.restore();    
-		}
+		const { ctx } = this;
+		const { drawImage } = ctx.helper;
+		const logo = path.join(__dirname, '../public/assets/image/', 'seal.png');
+		drawImage(canvasCtx, { x: 5.5 * cellWidth, y: 200 + 5.5 * cellHeight, width: 200, height: 200, src: logo });
 	}
 
 	// 生成pdf并用base64编码
